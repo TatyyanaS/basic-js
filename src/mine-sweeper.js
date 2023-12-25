772c9f36962ee5592b3db94d5a544dbf69ffbd5e
TatyyanaS <taty.yana.kz@gmail.com>
2023-12-26 03:27:27 +0600

    done

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,31 +1,38 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
+    throw new Error('Invalid input');
+  }
+
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  const result = [];
+
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      let count = 0;
+
+      for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+          const ni = i + x;
+          const nj = j + y;
+
+          if (ni >= 0 && ni < rows && nj >= 0 && nj < cols) {
+            count += matrix[ni][nj] ? 1 : 0;
+          }
+        }
+      }
+
+      row.push(matrix[i][j] ? 1 : count);
+    }
+
+    result.push(row);
+  }
+
+  return result;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,32 +1,47 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
- */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let shift = (this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(key[keyIndex])) % this.alphabet.length;
+        result += this.alphabet[shift];
+        keyIndex = ++keyIndex % key.length;
+      } else {
+        result += message[i];
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (this.alphabet.includes(encryptedMessage[i])) {
+        let shift = (this.alphabet.indexOf(encryptedMessage[i]) + this.alphabet.length - this.alphabet.indexOf(key[keyIndex])) % this.alphabet.length;
+        result += this.alphabet[shift];
+        keyIndex = ++keyIndex % key.length;
+      } else {
+        result += encryptedMessage[i];
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
